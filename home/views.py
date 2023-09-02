8ca69430eb6548d6c36405c8f3a6f5d3bb9b6bfb
amankr57451 <aman057451@gmail.com>
2023-09-02 22:40:01 +0530

    My first commit

---
+++ home/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import os
+import openai
+
+openai.api_key = "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"
+
+
+# Create your views here.
+def chat(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def chatAPI(request):
+    if request.method == "POST":
+       prompt=request.POST["prompt"]
+       response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+                
+       print(response)
+       return JsonResponse(response)
+    
+    return HttpResponse("Bad request")
